455b858841d22a6e555db7f447da5b1154add551
Amanda <amandaalouise@live.com>
2017-04-15 15:20:21 -0300

    Finaliza Cadastro

--- app/src/main/java/com/example/amand/projetointegrador/FinalizaCadastroActivity.java
+++ app/src/main/java/com/example/amand/projetointegrador/FinalizaCadastroActivity.java
@@ -1,13 +1,48 @@
 package com.example.amand.projetointegrador;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class FinalizaCadastroActivity extends AppCompatActivity {
+
+    private ImageView imgFinalizaCadastro;
+    private FloatingActionButton uploadImageButton;
+    private EditText finalizaCadastroTelefone;
+    private EditText finalizaCadastroCelular;
+    private EditText finalizaCadastroFacebook;
+    private EditText finalizaCadastroWhats;
+    private Button finalizaCadastroBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finaliza_cadastro);
+
+        imgFinalizaCadastro = (ImageView) findViewById(R.id.imgFinalizaCadastro);
+        uploadImageButton = (FloatingActionButton) findViewById(R.id.uploadImageButton);
+        finalizaCadastroTelefone = (EditText) findViewById(R.id.finalizaCadastroTelefone);
+        finalizaCadastroCelular = (EditText) findViewById(R.id.finalizaCadastroCelular);
+        finalizaCadastroFacebook = (EditText) findViewById(R.id.finalizaCadastroFacebook);
+        finalizaCadastroWhats = (EditText) findViewById(R.id.finalizaCadastroWhats);
+        finalizaCadastroBtn = (Button) findViewById(R.id.finalizaCadastroBtn);
+
+        //https://github.com/ArthurHub/Android-Image-Cropper/wiki/Pick-image-for-cropping-from-Camera-or-Gallery
+
+        uploadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
+
 }
